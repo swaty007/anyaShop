353811ac729d7a2995992ba049f1ffdc1e5b5912
swaty <swaty0007@gmail.com>
2021-09-01 00:15:38 +0300

    mobile menu update

--- wp-content/themes/anya/header.php
+++ wp-content/themes/anya/header.php
@@ -75,7 +75,7 @@ if (function_exists('wp_body_open')) {
                         <input id="navbar__search--input" class="navbar__search--input" type="text" name="s" placeholder="<?php pll_e('Поиск');?>">
                     </form>
                 </li>
-                <li class="menu-item d-flex d-lg-none align-items-center transition-3s">
+                <li id="menu__btn" class="menu-item d-flex d-lg-none align-items-center transition-3s">
                     <div class="icon">
                         <!-- <i class="fas fa-bars"></i> -->
                         <span class="transition-3s"></span>
@@ -157,210 +157,36 @@ if (function_exists('wp_body_open')) {
         <div class="close-btn">
             <i class="fas fa-times"></i>
         </div>
-        <ul class="menu-list d-block">
-            <li class="link relative d-block">
-                <div class="title">
-                    <a class="transition-2s" href="#">Объективы</a>
-                    <span class="open-list-btn pull-right">
-						<i class="fas fa-plus show transition-5s"></i>
-						<i class="fas fa-minus hide transition-5s"></i>
-					</span>
-                </div>
-                <ul class="under-list transition-5s">
-                    <li><a class="transition-2s" href="#">Серия Di (полноразмерная матрица)</a></li>
-                    <li><a class="transition-2s" href="#">Серия Di II (APS-C)</a></li>
-                    <li><a class="transition-2s" href="#">Серия Di III (для цифровых системных камер</a></li>
-                </ul>
-            </li>
-            <li class="link relative d-block">
-                <div class="title">
-                    <a class="transition-2s" href="#">Студийный свет</a>
-                    <span class="open-list-btn pull-right">
-						<i class="fas fa-plus show transition-5s"></i>
-						<i class="fas fa-minus hide transition-5s"></i>
-					</span>
-                </div>
-                <ul class="under-list transition-5s">
-                    <li><a class="transition-2s" href="#">Rime Lite i.TTL</a></li>
-                    <li><a class="transition-2s" href="#">Rime Lite Ni</a></li>
-                    <li><a class="transition-2s" href="#">Rime Lite i</a></li>
-                    <li><a class="transition-2s" href="#">Rime Lite Fame Plus</a></li>
-                    <li><a class="transition-2s" href="#">Rime Lite XB-Prime</a></li>
-                    <li><a class="transition-2s" href="#">Rime Lite F Plus</a></li>
-                    <li><a class="transition-2s" href="#">Комплекты студийного света</a></li>
-                    <li><a class="transition-2s" href="#">Сумки для вспышек</a></li>
-                    <li><a class="transition-2s" href="#">Аксессуары</a></li>
-                </ul>
-            </li>
-            <li class="link relative d-block">
-                <div class="title">
-                    <a class="transition-2s" href="#">Штативные головки</a>
-                </div>
-                <ul class="under-list transition-5s"></ul>
-            </li>
-            <li class="link relative d-block">
-                <div class="title">
-                    <a class="transition-2s" href="#">Радиосинхронизаторы и ДУ</a>
-                </div>
-                <ul class="under-list transition-5s"></ul>
-            </li>
-            <li class="link relative d-block">
-                <div class="title">
-                    <a class="transition-2s" href="#">Аксессуары для вспышек</a>
-                </div>
-                <ul class="under-list transition-5s"></ul>
-            </li>
-            <li class="link relative d-block">
-                <div class="title">
-                    <a class="transition-2s" href="#">Светофильтры</a>
-                    <span class="open-list-btn pull-right">
-						<i class="fas fa-plus show transition-5s"></i>
-						<i class="fas fa-minus hide transition-5s"></i>
-					</span>
-                </div>
-                <ul class="under-list transition-5s">
-                    <li><a class="transition-2s" href="#">Защитные</a></li>
-                    <li><a class="transition-2s" href="#">Поляризационные</a></li>
-                    <li><a class="transition-2s" href="#">Нейтральные</a></li>
-                    <li><a class="transition-2s" href="#">Макролинзы</a></li>
-                    <li><a class="transition-2s" href="#">Смягчающие</a></li>
-                    <li><a class="transition-2s" href="#">Лучевые, Эффектные</a></li>
-                    <li><a class="transition-2s" href="#">Градиентные</a></li>
-                    <li><a class="transition-2s" href="#">Конвертеры</a></li>
-                    <li><a class="transition-2s" href="#">Аксессуары</a></li>
-                </ul>
-            </li>
-            <li class="link relative d-block">
-                <div class="title">
-                    <a class="transition-2s" href="#">Фоны</a>
-                    <span class="open-list-btn pull-right">
-						<i class="fas fa-plus show transition-5s"></i>
-						<i class="fas fa-minus hide transition-5s"></i>
-					</span>
-                </div>
-                <ul class="under-list transition-5s">
-                    <li><a class="transition-2s" href="#">Фоны тканевые </a></li>
-                    <li><a class="transition-2s" href="#">Фоны бумажные, виниловые </a></li>
-                    <li><a class="transition-2s" href="#">Фоны двухсторонние складные </a></li>
-                    <li><a class="transition-2s" href="#">Фоны Chromakey </a></li>
-                </ul>
-            </li>
-            <li class="link relative d-block">
-                <div class="title">
-                    <a class="transition-2s" href="#">Боксы для макросъемки</a>
-                </div>
-                <ul class="under-list transition-5s"></ul>
-            </li>
-            <li class="link relative d-block">
-                <div class="title">
-                    <a class="transition-2s" href="#">Стойки</a>
-                </div>
-                <ul class="under-list transition-5s"></ul>
-            </li>
-            <li class="link relative d-block">
-                <div class="title">
-                    <a class="transition-2s" href="#">Зонты</a>
-                </div>
-                <ul class="under-list transition-5s"></ul>
-            </li>
-            <li class="link relative d-block">
-                <div class="title">
-                    <a class="transition-2s" href="#">Штативы</a>
-                    <span class="open-list-btn pull-right">
-						<i class="fas fa-plus show transition-5s"></i>
-						<i class="fas fa-minus hide transition-5s"></i>
-					</span>
-                </div>
-                <ul class="under-list transition-5s">
-                    <li><a class="transition-2s" href="#">Штативы профессиональные </a></li>
-                    <li><a class="transition-2s" href="#">Штативы без головок </a></li>
-                    <li><a class="transition-2s" href="#">Штативы видео </a></li>
-                    <li><a class="transition-2s" href="#">Штативы любительские </a></li>
-                    <li><a class="transition-2s" href="#">Штативы Sprint </a></li>
-                    <li><a class="transition-2s" href="#">Аксессуары </a></li>
-                </ul>
-            </li>
-            <li class="link relative d-block">
-                <div class="title">
-                    <a class="transition-2s" href="#">Рефлекторы</a>
-                    <span class="open-list-btn pull-right">
-						<i class="fas fa-plus show transition-5s"></i>
-						<i class="fas fa-minus hide transition-5s"></i>
-					</span>
-                </div>
-                <ul class="under-list transition-5s">
-                    <li><a class="transition-2s" href="#">Аксессуары </a></li>
-                </ul>
-            </li>
-            <li class="link relative d-block">
-                <div class="title">
-                    <a class="transition-2s" href="#">Столы для предметной съемки</a>
-                </div>
-                <ul class="under-list transition-5s"></ul>
-            </li>
-            <li class="link relative d-block">
-                <div class="title">
-                    <a class="transition-2s" href="#">Наборы для чистки</a>
-                </div>
-                <ul class="under-list transition-5s"></ul>
-            </li>
-            <li class="link relative d-block">
-                <div class="title">
-                    <a class="transition-2s" href="#">Аксессуары</a>
-                </div>
-                <ul class="under-list transition-5s"></ul>
-            </li>
-            <li class="link relative d-block">
-                <div class="title">
-                    <a class="transition-2s" href="#">Софтбоксы</a>
-                    <span class="open-list-btn pull-right">
-						<i class="fas fa-plus show transition-5s"></i>
-						<i class="fas fa-minus hide transition-5s"></i>
-					</span>
-                </div>
-                <ul class="under-list transition-5s">
-                    <li><a class="transition-2s" href="#">Узкие софтбоксы </a></li>
-                    <li><a class="transition-2s" href="#">Восьмиугольные софтбоксы </a></li>
-                    <li><a class="transition-2s" href="#">Прямоугольные софтбоксы </a></li>
-                    <li><a class="transition-2s" href="#">Квадратные софтбоксы </a></li>
-                    <li><a class="transition-2s" href="#">Grand Box софтбоксы </a></li>
-                    <li><a class="transition-2s" href="#">Portable Box софтбоксы </a></li>
-                    <li><a class="transition-2s" href="#">Аксессуары </a></li>
-                </ul>
-            </li>
-            <li class="link relative d-block">
-                <div class="title">
-                    <a class="transition-2s" href="#">Моноподы</a>
-                </div>
-                <ul class="under-list transition-5s"></ul>
-            </li>
-            <li class="link relative d-block">
-                <div class="title">
-                    <a class="transition-2s" href="#">Соты</a>
-                </div>
-                <ul class="under-list transition-5s"></ul>
-            </li>
-            <li class="link relative d-block">
-                <div class="title">
-                    <a class="transition-2s" href="#">Отражатели</a>
-                </div>
-                <ul class="under-list transition-5s"></ul>
-            </li>
-            <li class="link relative d-block pages-navigation">
-                <div class="title">
-                    <a class="transition-2s" href="#">Навигация</a>
-                    <span class="open-list-btn pull-right">
-						<i class="fas fa-plus show transition-5s"></i>
-						<i class="fas fa-minus hide transition-5s"></i>
-					</span>
-                </div>
-                <ul class="under-list transition-5s">
-                    <li><a class="transition-2s" href="#">Про компанию </a></li>
-                    <li><a class="transition-2s" href="#">Публикации </a></li>
-                    <li><a class="transition-2s" href="#">Контакты </a></li>
-                </ul>
-            </li>
-        </ul>
+        <?php
+        wp_nav_menu([
+            'theme_location' => 'menu-header',
+            'menu_id' => 'header_menu_mobile',
+            'container' => 'ul',
+            'menu_class' => 'menu-list d-block',
+        ]);
+        ?>
+<!--        <ul class="menu-list d-block">-->
+<!--            <li class="link relative d-block">-->
+<!--                <div class="title">-->
+<!--                    <a class="transition-2s" href="#">Объективы</a>-->
+<!--                    <span class="open-list-btn pull-right">-->
+<!--						<i class="fas fa-plus show transition-5s"></i>-->
+<!--						<i class="fas fa-minus hide transition-5s"></i>-->
+<!--					</span>-->
+<!--                </div>-->
+<!--                <ul class="under-list transition-5s">-->
+<!--                    <li><a class="transition-2s" href="#">Серия Di (полноразмерная матрица)</a></li>-->
+<!--                    <li><a class="transition-2s" href="#">Серия Di II (APS-C)</a></li>-->
+<!--                    <li><a class="transition-2s" href="#">Серия Di III (для цифровых системных камер</a></li>-->
+<!--                </ul>-->
+<!--            </li>-->
+<!--            <li class="link relative d-block">-->
+<!--                <div class="title">-->
+<!--                    <a class="transition-2s" href="#">Штативные головки</a>-->
+<!--                </div>-->
+<!--                <ul class="under-list transition-5s"></ul>-->
+<!--            </li>-->
+<!--        </ul>-->
     </div>
 </section>
 
